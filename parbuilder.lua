4248ec4d5ca8afe43d4bf3daaa253f2eba1573a7
Michal Hoftich <michal.h21@gmail.com>
2014-11-14 14:29:48 +0100

    Deal with groupcodes

--- parbuilder.lua
+++ parbuilder.lua
@@ -4,8 +4,14 @@ local persistence = require "realpgno-persistence.lua"
 
 local paragraphs = {}
 
-function Parbuilder.add(par)
-	table.insert(paragraphs, par)
+local suppress_groupcodes = {
+	insert = true
+}
+function Parbuilder.add(par, groupcode)
+	local suppress = suppress_groupcodes[groupcode]
+	if not suppress then
+  	table.insert(paragraphs, par)
+	end
 end
 
 function Parbuilder.save(filename)
